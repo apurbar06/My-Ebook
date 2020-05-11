6c1d1baf99cf7f2f61acad899c352f0443e2d283
Apurba Roy <apurbar06@gmail.com>
2020-05-11 16:47:44 +0530

    change first spinner functionality

--- app/src/main/java/com/example/myebook/MainActivity.java
+++ app/src/main/java/com/example/myebook/MainActivity.java
@@ -3,19 +3,21 @@ package com.example.myebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private Spinner mSpinnerGroup;
     private Spinner mSpinnerSemester;
+    private int mSpinnerGroupPos;
     private static final String[] mGroupArray = {"CED", "MPD", "ESD", "CSE", "MSM"};
     private static final String[] mSemesterArrayDual = {"First", "Second", "Third", "Forth", "Fifth"};
-    private static final String[] mSemesterArraySingle = {"First", "Second", "Third", "Forth", "Fifth"};
+    private static final String[] mSemesterArraySingle = {"First", "Second", "Third", "Forth"};
 
 
     @Override
@@ -29,16 +31,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mGroupArray);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinnerGroup.setAdapter(adapter);
-        mSpinnerGroup.setOnItemSelectedListener(this);
+        mSpinnerGroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                mSpinnerGroupPos = position;
+                Log.d(TAG, "onItemSelected: position " + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
