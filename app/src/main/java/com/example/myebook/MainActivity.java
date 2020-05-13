49b6ba5b9b91cc4a093e507647f108f841a0da1e
Apurba Roy <apurbar06@gmail.com>
2020-05-13 17:56:52 +0530

    add graduation level spinner

--- app/src/main/java/com/example/myebook/MainActivity.java
+++ app/src/main/java/com/example/myebook/MainActivity.java
@@ -12,14 +12,22 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private Spinner mSpinnerGroup;
+    private Spinner mSpinnerGraduationLevel;
+    private Spinner mSpinnerCourse;
     private Spinner mSpinnerSemester;
-    private int mSpinnerGroupPos;
-    private String mGroup;
+    private int mSpinnerGraduationLevelPos;
+    private int mSpinnerCoursePos;
+    private String mGraduationLevel;
+    private String mCourse;
     private String mSemester;
-    private static final String[] mGroupArray = {"CED", "MPD", "ESD", "CSE", "MSM"};
-    private static final String[] mSemesterArrayDual = {"First", "Second", "Third", "Forth", "Fifth"};
-    private static final String[] mSemesterArraySingle = {"First", "Second", "Third", "Forth"};
+    private static final String[] mGraduationLevelArray = {"UG", "DD", "PG"};
+    private static final String[] mUgCourseArray = {"COE", "EDM", "MDM", "MSM"};
+    private static final String[] mDdCourseArray = {"CED", "EVD", "ESD", "MPD", "MFD"};
+    private static final String[] mPgCourseArray = {"CES", "EDS", "MDS", "SMT"};
+    private static final String[] mUgSemesterArray = {"First", "Second"};
+    private static final String[] mDdSemesterArray = {"First", "Second"};
+    private static final String[] mPgSemesterArray = {"First", "Second"};
+
 
 
     @Override
@@ -27,33 +35,77 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSpinnerGroup = (Spinner)findViewById(R.id.spinner_group);
+        mSpinnerGraduationLevel = (Spinner)findViewById(R.id.spinner_graduation_level);
+        mSpinnerCourse = (Spinner)findViewById(R.id.spinner_course);
         mSpinnerSemester = (Spinner)findViewById(R.id.spinner_semester);
 
 
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mGroupArray);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinnerGroup.setAdapter(adapter);
-        mSpinnerGroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        ArrayAdapter<String>adapterGL = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mGraduationLevelArray);
+        adapterGL.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerGraduationLevel.setAdapter(adapterGL);
+        mSpinnerGraduationLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                mSpinnerGroupPos = position;
-                mGroup = mSpinnerGroup.getSelectedItem().toString();
+                mSpinnerGraduationLevelPos = position;
+                mGraduationLevel = mSpinnerGraduationLevel.getSelectedItem().toString();
 
+                if (mSpinnerGraduationLevelPos == 0) {
 
-                if (mSpinnerGroupPos == 0){
-
-                    ArrayAdapter<String>adapter1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mSemesterArrayDual);
+                    ArrayAdapter<String>adapter1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mUgCourseArray);
                     adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    mSpinnerSemester.setAdapter(adapter1);
+                    mSpinnerCourse.setAdapter(adapter1);
+
+                } else if (mSpinnerGraduationLevelPos == 1) {
+
+                    ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mDdCourseArray);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    mSpinnerCourse.setAdapter(adapter2);
 
                 } else {
 
-                    ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mSemesterArraySingle);
+                    ArrayAdapter<String>adapter3 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mPgCourseArray);
+                    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    mSpinnerCourse.setAdapter(adapter3);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+        mSpinnerCourse.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                mSpinnerCoursePos = position;
+                mCourse = mSpinnerCourse.getSelectedItem().toString();
+
+
+                if (mSpinnerGraduationLevelPos == 0){
+
+                    ArrayAdapter<String>adapter1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mUgSemesterArray);
+                    adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    mSpinnerSemester.setAdapter(adapter1);
+
+                } else if (mSpinnerGraduationLevelPos ==1) {
+
+                    ArrayAdapter<String>adapter2 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mDdSemesterArray);
                     adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     mSpinnerSemester.setAdapter(adapter2);
 
+                } else {
+
+                    ArrayAdapter<String>adapter3 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mPgSemesterArray);
+                    adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    mSpinnerSemester.setAdapter(adapter3);
                 }
             }
 
@@ -68,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 mSemester = mSpinnerSemester.getSelectedItem().toString();
-                Log.d(TAG, "onItemSelected: " + mGroup + mSemester);
+
             }
 
             @Override
@@ -79,4 +131,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void setGlCourseSemester(View view) {
+
+        Log.d(TAG, "onItemSelected: " + mGraduationLevel + mCourse + mSemester);
+    }
 }
