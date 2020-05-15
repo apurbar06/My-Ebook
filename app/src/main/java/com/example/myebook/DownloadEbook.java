cf0fc5728f74f93e9f69eb424732176a3ac24f56
Apurba Roy <apurbar06@gmail.com>
2020-05-15 11:23:24 +0530

    add DownloadEbook activity

---
+++ app/src/main/java/com/example/myebook/DownloadEbook.java
@@ -0,0 +1,14 @@
+package com.example.myebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DownloadEbook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_ebook);
+    }
+}
